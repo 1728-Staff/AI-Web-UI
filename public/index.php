cbcd2a9564e4843393dcbce928042d9f5ed0cbc4
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-03-18 17:37:37 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
-
+<?php include '../templates/header.php'; ?>
+<div id="chat-container">
+    <div id="chat-box"></div>
+    <input type="text" id="user-input" placeholder="Type your message...">
+    <button id="send-btn">Send</button>
+</div>
+<script src="script.js"></script>
+<?php include '../templates/footer.php'; ?>
