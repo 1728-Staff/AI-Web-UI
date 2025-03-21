8c557ed4b9732025a15acc2fe9185da711031d32
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-03-21 09:37:34 -0600

    Imported company footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,1 +1,11 @@
-
+<footer class="py-2 bg-zinc-800">
+        <div class="container mx-auto text-center container-mobile">
+            <div class="custom-banner mb-2">
+                 </div>
+            <div class="footer-links space-x-2 mb-2">
+                <a href="https://www.1728studios.com/about" target="_blank" class="hover:text-zinc-300 hover-effect text-sm">About 1728Studios</a>
+            </div>
+            <p class="text-sm">© Printers Only Fans Brought to you by 1728studios.com</p>
+        </div>
+    </footer>
+    <script src='background.js'></script>
