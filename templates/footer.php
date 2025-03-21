a94950d0e8b2f9a0a363c3313c2c44aff55ed684
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-03-21 09:39:03 -0600

    Removed Printers Only Fans reference

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
             <div class="footer-links space-x-2 mb-2">
                 <a href="https://www.1728studios.com/about" target="_blank" class="hover:text-zinc-300 hover-effect text-sm">About 1728Studios</a>
             </div>
-            <p class="text-sm">© Printers Only Fans Brought to you by 1728studios.com</p>
+            <p class="text-sm">© Brought to you by 1728studios.com</p>
         </div>
     </footer>
     <script src='background.js'></script>
