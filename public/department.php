3522645b9869825acbd6be533961d4e2a110c2b7
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-03-21 09:55:24 -0600

    Create department.php

---
+++ public/department.php
@@ -0,0 +1,21 @@
+<?php include '../templates/header.php'; ?>
+
+<div id="department-wrapper">
+    <h2>Select Your Department</h2>
+    <div class="department-list">
+        <div class="department-option">
+            <span class="bullet">&#8226;</span>
+            <a href="login_accounting.php" class="dept-button">Accounting</a>
+        </div>
+        <div class="department-option">
+            <span class="bullet">&#8226;</span>
+            <a href="login_finance.php" class="dept-button">Finance</a>
+        </div>
+        <div class="department-option">
+            <span class="bullet">&#8226;</span>
+            <a href="login_hr.php" class="dept-button">Human Resources</a>
+        </div>
+    </div>
+</div>
+
+<?php include '../templates/footer.php'; ?>
