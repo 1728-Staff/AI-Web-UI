5e0c679013afe9989a6ab1949762b0a49455f067
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-03-29 13:28:52 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,10 +2,6 @@
 
 <div id="landing-wrapper">
     <div class="landing-content">
-        <h1 class="system-title">SynapseIQ</h1>
-        <p class="system-tagline">
-            Intelligent access to your ERP insights, right at your fingertips.
-        </p>
         <a href="department.php" class="start-button">Let's Get Started</a>
     </div>
 </div>
