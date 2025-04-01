c70597d47e89b0f9efdf00c3af527240578e5449
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-04-01 08:39:31 -0600

    Update header.php
    adjusted path

--- templates/header.php
+++ templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>SynapseIQ | Intelligent ERP Interface</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header class="top-bar">
