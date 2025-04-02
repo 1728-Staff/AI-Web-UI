20db277b74332c51dcb5b29349af8cb58694206b
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-04-02 09:13:40 -0600

    Update index.php
    Restart for error resolution

--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
+<?php include '../templates/header.php'; ?>
 
+<div id="landing-wrapper">
+    <div class="landing-content">
+        <a href="department.php" class="start-button">Let's Get Started</a>
+    </div>
+</div>
+
+<?php include '../templates/footer.php'; ?>
