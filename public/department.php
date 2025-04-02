9c38a69e34fc5c5129b462fe9d71e0dc8040cdca
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-04-02 14:11:33 -0600

    Update department.php
    Updated for new layout

--- public/department.php
+++ public/department.php
@@ -2,20 +2,18 @@
 
 <div id="department-wrapper">
     <h2>Select Your Department</h2>
-    
+
     <div class="department-list">
         <div class="department-option">
-            <span class="bullet">&#8226;</span>
+            <span class="bullet">•</span>
             <a href="login_accounting.php" class="dept-button">Accounting</a>
         </div>
-        
         <div class="department-option">
-            <span class="bullet">&#8226;</span>
+            <span class="bullet">•</span>
             <a href="login_finance.php" class="dept-button">Finance</a>
         </div>
-        
         <div class="department-option">
-            <span class="bullet">&#8226;</span>
+            <span class="bullet">•</span>
             <a href="login_hr.php" class="dept-button">Human Resources</a>
         </div>
     </div>
