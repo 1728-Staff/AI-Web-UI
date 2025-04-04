0b05cc8f13c6de452c1d87cd9f9099054d1800fc
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-04-04 10:47:31 -0600

    Update login_finance.php
    removed extension for apache

--- public/login_finance.php
+++ public/login_finance.php
@@ -1,7 +1,7 @@
 <?php include '../templates/header.php'; ?>
 
 <div id="login-wrapper">
-    <form class="login-form" method="POST" action="verify_finance.php">
+    <form class="login-form" method="POST" action="verify_finance">
         <h2>Login Template</h2>
 
         <input type="text" name="username" placeholder="Username" required>
