af5efbb2eaf63a62840a190cf742adac5bc3efcd
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-04-04 09:12:47 -0600

    Update verify_accounting.php
    re-added debug for auth:
    file_put_contents("login_debug.log", "USERNAME=[$username] | PASSWORD=[$password]\n", FILE_APPEND);

--- public/verify_accounting.php
+++ public/verify_accounting.php
@@ -8,6 +8,8 @@ if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 }
 
+file_put_contents("login_debug.log", "USERNAME=[$username] | PASSWORD=[$password]\n", FILE_APPEND);
+
 // Sanitize user input
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
