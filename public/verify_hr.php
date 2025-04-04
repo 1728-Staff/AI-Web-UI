2c6e3b0b21ea0246370084240f3b1a17dd11e926
1728-Staff <164283458+1728-Staff@users.noreply.github.com>
2025-04-04 10:53:05 -0600

    Update verify_hr.php

--- public/verify_hr.php
+++ public/verify_hr.php
@@ -3,7 +3,7 @@ session_start();
 
 // Block if no POST data
 if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['username'], $_POST['password'])) {
-    header("Location: login_accounting.php?error=invalidrequest");
+    header("Location: login_hr.php?error=invalidrequest");
     exit();
 }
 // Connect to MySQL
@@ -17,7 +17,7 @@ if ($mysqli->connect_error) {
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
 
-file_put_contents("login_debug.log", "USERNAME=[$username] | PASSWORD=[$password]\n", FILE_APPEND);
+file_put_contents("login_hr_debug.log", "USERNAME=[$username] | PASSWORD=[$password]\n", FILE_APPEND);
 
 // Prepare and execute SQL query
 $stmt = $mysqli->prepare("SELECT password_hash FROM hr_users WHERE username = ?");
@@ -37,12 +37,12 @@ if ($stmt->num_rows === 1) {
         exit();
     } else {
         // Wrong password
-        header("Location: login_accounting.php?error=invalid");
+        header("Location: login_hr.php?error=invalid");
         exit();
     }
 } else {
     // No such user
-    header("Location: login_accounting.php?error=notfound");
+    header("Location: login_hr.php?error=notfound");
     exit();
 }
 
